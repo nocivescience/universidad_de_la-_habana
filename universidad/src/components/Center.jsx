bfa0afa50d757cb2c393328a8a003a0eda02c9bb
noceive <nocivescience@gmail.com>
2023-01-11 20:01:19 -0300

    se enrriela mejor

--- universidad/src/components/Center.jsx
+++ universidad/src/components/Center.jsx
@@ -1,10 +1,13 @@
 import '../static/css/Center.css'
 import { CenterComponent } from './CenterComponent'
+import ArticuloLogo from '../assets/articulo-logo.jpg'
 export const Center=()=>{
     return (
         <div className='center-container'>
             <CenterComponent
-                
+                title='prueba 1'
+                parrafo='Minim ipsum consectetur ex quis excepteur Lorem qui quis quis enim exercitation elit ut. Tempor exercitation nisi mollit cupidatat et commodo Lorem ea et minim commodo tempor cupidatat cupidatat. Proident excepteur non incididunt duis cupidatat ad minim ipsum cillum ipsum irure ipsum velit Lorem. Anim Lorem voluptate esse reprehenderit aliquip sit sunt est enim. Aliquip anim anim officia laboris proident laboris ut anim voluptate enim mollit.'
+                imagenes={[ArticuloLogo]}
             ></CenterComponent>
         </div>
     )
--- universidad/src/components/CenterComponent.jsx
+++ universidad/src/components/CenterComponent.jsx
@@ -2,10 +2,14 @@ import '../static/css/center-component.css'
 export const CenterComponent=(props)=>{
     return (
         <div className='article-container'>
-            <h2 className='title display-6'>{props.title}</h2>
             <div className='interior'>
-                <img src={props.image}></img>
-                <p>{props.parrafo}</p>
+                <div className='container-images'>
+                    {props.imagenes.map(imagen=>(<img src={imagen}></img>))}
+                </div>
+                <div className='textos'>
+                    <h2 className='title display-6'>{props.title}</h2>
+                    <p>{props.parrafo}</p>
+                </div>
             </div>
         </div>
     )
