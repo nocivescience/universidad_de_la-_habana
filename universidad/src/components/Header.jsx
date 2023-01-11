6b543af8d3d2b2855e255951a0e247de9e5d6418
noceive <nocivescience@gmail.com>
2023-01-11 03:09:37 -0300

    navbar perfecto

---
+++ universidad/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import '../static/css/Header.css'
+import Portal from '../assets/university-library.jpg'
+export const Header=()=>{
+    return (
+        <div>
+            
+        </div>
+    )
+}
