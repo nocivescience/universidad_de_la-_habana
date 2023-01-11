b8ef5261d38d815984804b94478f00f1d14326c4
noceive <nocivescience@gmail.com>
2023-01-11 19:03:47 -0300

    ya cree los elementos vacios del centro

--- universidad/src/components/Center.jsx
+++ universidad/src/components/Center.jsx
@@ -1,6 +1,11 @@
 import '../static/css/Center.css'
+import { CenterComponent } from './CenterComponent'
 export const Center=()=>{
     return (
-        <div className='segundo-elemento'>hola</div>
+        <div className='center-container'>
+            <CenterComponent
+                
+            ></CenterComponent>
+        </div>
     )
 }
--- universidad/src/components/CenterComponent.jsx
+++ universidad/src/components/CenterComponent.jsx
@@ -1,8 +1,12 @@
-import '../static/css/CenterComponent.sass'
-export const CenterComponent=()=>{
+import '../static/css/center-component.css'
+export const CenterComponent=(props)=>{
     return (
-        <div>
-            
+        <div className='article-container'>
+            <h2 className='title display-6'>{props.title}</h2>
+            <div className='interior'>
+                <img src={props.image}></img>
+                <p>{props.parrafo}</p>
+            </div>
         </div>
     )
 }
