29912852d5b73b640540d2136718d4b29eec7554
noceive <nocivescience@gmail.com>
2023-01-11 17:45:39 -0300

    funciona bien el componente a la izquierda

--- universidad/src/components/Left.jsx
+++ universidad/src/components/Left.jsx
@@ -1,6 +1,20 @@
 import '../static/css/Left.css'
+import { LeftComponent } from './LeftComponent'
 export const Left=()=>{
     return (
-        <div className='primer-elemento'></div>
+        <div className='primer-elemento'>
+            <LeftComponent
+                titulo='CONÓZCANOS'
+                lista={['Bienvenido','Acerca de la UCMH','Historia de la UCMH']}
+            ></LeftComponent>
+            <LeftComponent
+                titulo='GENERAL UCMH'
+                lista={['Dirección de cuadros','Secretaría general','Dirección de Relaciones Internacionales']}
+            ></LeftComponent>
+            <LeftComponent
+                titulo='GENERAL UCMH'
+                lista={['Dirección de cuadros','Secretaría general','Dirección de Relaciones Internacionales']}
+            ></LeftComponent>
+        </div>
     )
 }
