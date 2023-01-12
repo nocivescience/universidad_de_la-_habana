bfc94255f35162ca9daffedeed725a6be7c6c3ef
noceive <nocivescience@gmail.com>
2023-01-12 00:23:08 -0300

    va super

--- universidad/src/components/Center.jsx
+++ universidad/src/components/Center.jsx
@@ -1,14 +1,23 @@
-import '../static/css/Center.css'
-import { CenterComponent } from './CenterComponent'
-import ArticuloLogo from '../assets/articulo-logo.jpg'
-export const Center=()=>{
-    return (
-        <div className='center-container'>
-            <CenterComponent
-                title='prueba 1'
-                parrafo='Minim ipsum consectetur ex quis excepteur Lorem qui quis quis enim exercitation elit ut. Tempor exercitation nisi mollit cupidatat et commodo Lorem ea et minim commodo tempor cupidatat cupidatat. Proident excepteur non incididunt duis cupidatat ad minim ipsum cillum ipsum irure ipsum velit Lorem. Anim Lorem voluptate esse reprehenderit aliquip sit sunt est enim. Aliquip anim anim officia laboris proident laboris ut anim voluptate enim mollit.'
-                imagenes={[ArticuloLogo]}
-            ></CenterComponent>
-        </div>
-    )
-}
+import "../static/css/Center.css";
+import { CenterComponent } from "./CenterComponent";
+import ArticuloLogo from "../assets/articulo-logo.jpg";
+import PersonasRecibiendoDiplomas from "../assets/personas-recibiendo-diplomas.jpg";
+import FotoAntigua from "../assets/foto-antigua.jpg";
+import PersonasSentadas from "../assets/personas-sentadas.jpg";
+export const Center = () => {
+  return (
+    <div className="center-container">
+      <CenterComponent
+        title="Pesquisador Virtual COVID 19"
+        parrafo="Nueva herramienta que permite captar información sobre el estado de salud de la población"
+        imagenes={[ArticuloLogo]}
+      ></CenterComponent>
+      <CenterComponent
+        title="Acto por el 40 aniversario del destacamento Carlos Juan Finlay"
+        parrafo="Un 12 de marzo de 1982 quedó constituido el Destacamento de Ciencias Médicas Carlos Juan Finlay, en honor al eminente científico y médico descubridor del agente transmisor de la fiebre amarrilla.
+        Con la presencia de Jorge González Pérez, director de Docencia del Ministerio de Salud Pública, Mairim Lago Queija, rectora de la Universidad de Ciencias Médicas de la Habana y otros representantes de la universidad, dirigentes de la Unión de Jóvenes Comunistas, Federación Estudiantil Universitaria y Federación Estudiantil de la Enseñanza Media se realiza el acto por el 40 aniversario de la constitución del destacamento Carlos Juan Finlay."
+        imagenes={[PersonasRecibiendoDiplomas,FotoAntigua,PersonasSentadas]}
+      ></CenterComponent>
+    </div>
+  );
+};
